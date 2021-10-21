23761a4f3d8d3cd5202e1fef6c5443ec715f36eb
Paulo Rodrigues <paulo.rodrigues@rebase.com.br>
2021-10-20 23:47:28 -0300

    init slider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import GlobalStyles from './theme/GlobalStyles';
-import Card from './components/Card';
-import Jurassic from './assets/images/jurassic.png';
-import Madmax from './assets/images/madmax.png';
-import Rocky from './assets/images/rocky.png';
+import Slider from './components/Slider';
 
 function App() {
   return (
@@ -17,9 +14,7 @@ function App() {
           alignItems: 'center',
         }}
       >
-        <Card image={Jurassic} />
-        <Card image={Madmax} />
-        <Card image={Rocky} />
+        <Slider />
       </div>
     </>
   );
---
+++ src/components/Slider/styles.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const SliderImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+export const StyledSlider = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #fafafa;
+`;
